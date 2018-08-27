ef53581d9e4178670ad8c56ec9d61155abf9664d
robert roszak <robert.roszak@outlook.com>
2018-08-27 19:26:31 +0200

    Custom entry and custom picker are implemented

---
+++ Droid/BorderfulEntryRendererAndroid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using TestZXing;
+using TestZXing.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(BorderfulEntry),typeof(BorderfulEntryRendererAndroid))]
+namespace TestZXing.Droid
+{
+    public class BorderfulEntryRendererAndroid : EntryRenderer
+    {
+        public BorderfulEntryRendererAndroid(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+            {
+                var gd = new GradientDrawable();
+                gd.SetCornerRadius(60f);
+                gd.SetStroke(6, Android.Graphics.Color.DeepSkyBlue);
+                Control.SetBackground(gd);
+            }
+        }
+    }
+}
---
+++ Droid/BorderfulPickerRendererAndroid.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using TestZXing;
+using TestZXing.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(BorderfulPicker), typeof(BorderfulPickerRendererAndroid))]
+namespace TestZXing.Droid
+{
+    public class BorderfulPickerRendererAndroid : Xamarin.Forms.Platform.Android.AppCompat.PickerRenderer
+    {
+        public BorderfulPickerRendererAndroid(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetHintTextColor(Android.Graphics.Color.DeepSkyBlue);
+                Control.Gravity = GravityFlags.Center;
+                var gd = new GradientDrawable();
+                gd.SetCornerRadius(60f);
+                gd.SetStroke(6, Android.Graphics.Color.DeepSkyBlue);
+                Control.SetBackground(gd);
+            }
+        }
+    }
+}
