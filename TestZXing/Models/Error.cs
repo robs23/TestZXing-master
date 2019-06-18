fe07b1378f34e0e396c5b1851dc32ffa9c2e46fb
robert roszak <robert.roszak@outlook.com>
2019-06-18 10:44:29 +0200

    Xamarin.Essentials.Connectivity implemented to error tracing

--- TestZXing/Models/Error.cs
+++ TestZXing/Models/Error.cs
@@ -18,16 +18,52 @@ namespace TestZXing.Models
         public Error(Exception ex, string text, string methodName, string className)
         {
             string UserName = string.Empty;
+            string InternetConnectionStatus = "";
+            string ActiveConnections = "";
+
+            if (Connectivity.ConnectionProfiles.Contains(ConnectionProfile.Cellular))
+            {
+                ActiveConnections += "GSM ";
+            }
+            if (Connectivity.ConnectionProfiles.Contains(ConnectionProfile.WiFi))
+            {
+                ActiveConnections += "WiFi ";
+            }
+
             if(RuntimeSettings.CurrentUser != null)
             {
                 UserName = RuntimeSettings.CurrentUser.FullName;
             }
+            if(Connectivity.NetworkAccess == NetworkAccess.Internet)
+            {
+                InternetConnectionStatus = "Pełen internet";
+            }else if(Connectivity.NetworkAccess == NetworkAccess.ConstrainedInternet)
+            {
+                InternetConnectionStatus = "Ograniczone";
+            }else if(Connectivity.NetworkAccess == NetworkAccess.Local)
+            {
+                InternetConnectionStatus = "Lokalne";
+            }else if(Connectivity.NetworkAccess == NetworkAccess.None)
+            {
+                InternetConnectionStatus = "Brak";
+            }else if(Connectivity.NetworkAccess == NetworkAccess.Unknown)
+            {
+                InternetConnectionStatus = "Nieznane";
+            }
+            else
+            {
+                InternetConnectionStatus = "Tego przypadku powinno nie być... Prawdopodobnie przybyło opcji w  Connectivity.NetworkAcces.. Sprawdź Error.cs";
+            }
+
             var properties = new Dictionary<string, string>
                 {
                     {"Type", text},
                     {"Method", methodName},
                     {"Class", className},
-                    {"User", UserName}
+                    {"User", UserName},
+                    {"Połączenie internetowe", InternetConnectionStatus },
+                    {"Aktywne połączenia", ActiveConnections }
+
                 };
             Crashes.TrackError(ex, properties);
         }
