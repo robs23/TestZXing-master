930301224c44cb978e36d36d798693d199a28e31
robert roszak <robert.roszak@outlook.com>
2019-09-15 19:46:36 +0200

    Entity superclass and part model

---
+++ TestZXing/Models/Entity.cs
@@ -0,0 +1,87 @@
+﻿using ModernHttpClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TestZXing.Static;
+
+namespace TestZXing.Models
+{
+    public abstract class Entity<T>
+    {
+        public abstract int Id { get; set; }
+        public int CreatedBy { get; set; }
+        public string CreatedByName { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int? LmBy { get; set; }
+        public string LmByName { get; set; }
+        public DateTime? LmOn { get; set; }
+        public int TenantId { get; set; }
+        public string TenantName { get; set; }
+        public string AddedItem { get; set; }
+
+        public virtual async Task<string> Add()
+        {
+            using (var client = new HttpClient())
+            {
+                string _Result = "OK";
+                string url = Secrets.ApiAddress + $"Create{typeof(T).Name}?token=" + Secrets.TenantToken + "&UserId=" + RuntimeSettings.UserId;
+
+                try
+                {
+                    HttpClient httpClient = new HttpClient(new NativeMessageHandler() { Timeout = new TimeSpan(0, 0, 20), EnableUntrustedCertificates = true, DisableCaching = true });
+                    var serialized = JsonConvert.SerializeObject(this);
+                    var content = new StringContent(serialized, Encoding.UTF8, "application/json");
+                    HttpResponseMessage httpResponse = await Static.Functions.GetPostRetryAsync(() => httpClient.PostAsync(new Uri(url), content), TimeSpan.FromSeconds(3));
+                    if (!httpResponse.IsSuccessStatusCode)
+                    {
+                        _Result = httpResponse.ReasonPhrase;
+                    }
+                    else
+                    {
+                        var rString = await httpResponse.Content.ReadAsStringAsync();
+                        AddedItem = rString;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _Result = ex.Message;
+                    Static.Functions.CreateError(ex, "No connection", nameof(this.Add), this.GetType().Name);
+                }
+                return _Result;
+
+            }
+            
+        }
+
+        public async Task<string> Edit()
+        {
+            string url = Secrets.ApiAddress + $"Edit{typeof(T).Name}?token=" + Secrets.TenantToken + $"&id={this.Id}&UserId={RuntimeSettings.CurrentUser.UserId}";
+            string _Result = "OK";
+
+            try
+            {
+                HttpClient httpClient = new HttpClient(new NativeMessageHandler() { Timeout = new TimeSpan(0, 0, 20), EnableUntrustedCertificates = true, DisableCaching = true });
+                var serializedProduct = JsonConvert.SerializeObject(this);
+                var content = new StringContent(serializedProduct, Encoding.UTF8, "application/json");
+                HttpResponseMessage result = await Static.Functions.GetPostRetryAsync(() => httpClient.PutAsync(new Uri(url), content), TimeSpan.FromSeconds(3));
+                if (!result.IsSuccessStatusCode)
+                {
+                    _Result = result.ReasonPhrase;
+                }
+            }
+            catch (Exception ex)
+            {
+                _Result = ex.Message;
+                Static.Functions.CreateError(ex, "No connection", nameof(this.Edit), this.GetType().Name);
+            }
+
+
+            return _Result;
+        }
+
+    }
+}
---
+++ TestZXing/Models/Part.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestZXing.Models
+{
+    public class Part : Entity<Part>
+    {
+        public int PartId { get; set; }
+        public override int Id
+        {
+            set => value = PartId;
+            get => PartId;
+        }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string EAN { get; set; }
+        public Nullable<int> ProducerId { get; set; }
+        public string ProducerName { get; set; }
+        public string ProducentsCode { get; set; }
+        public Nullable<int> SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public string Symbol { get; set; }
+        public string Destination { get; set; }
+        public string Appliance { get; set; }
+        public string UsedOn { get; set; }
+        public string Token { get; set; }
+        public string Identifier
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(this.ProducentsCode))
+                {
+                    return this.ProducentsCode;
+                }
+                else if (!string.IsNullOrEmpty(this.Symbol))
+                {
+                    return this.Symbol;
+                }
+                else if (!string.IsNullOrEmpty(this.EAN))
+                {
+                    return this.EAN;
+                }
+                else
+                {
+                    return $"ID: {this.PartId}";
+                }
+            }
+        }
+    }
+}
