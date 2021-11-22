ce6970c57bc9fea875e7cfbb6ae3c3e3e4ed0250
Robert Roszak <robert.roszak@outlook.com>
2021-11-22 16:17:02 +0100

    With UserLog model

---
+++ TestZXing/Models/UserLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestZXing.Models
+{
+    public class UserLog: Entity<UserLog>
+    {
+        public override int Id
+        {
+            set => value = UserLogId;
+            get => UserLogId;
+        }
+        public int UserLogId { get; set; }
+        public string Platform { get; set; }
+        public string Device { get; set; }
+        public string LogName { get; set; }
+        public Nullable<bool> HasTheAppCrashed { get; set; }
+        public Nullable<bool> OnRequest { get; set; }
+        public string Comment { get; set; }
+    }
+}
---
+++ TestZXing/Models/UserLogKeeper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestZXing.Models
+{
+    public class UserLogKeeper : Keeper<UserLog>
+    {
+        protected override string ObjectName => "UserLog";
+
+        protected override string PluralizedObjectName => "UserLogs";
+    }
+}
